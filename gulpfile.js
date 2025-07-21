566e65021ee676f512c3a9514f8be22f80b6950b
worldofhope <n.sadova98@gmail.com>
2025-07-21 11:03:01 +0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
-// Основной модуль
 import gulp from "gulp";
-// Импорт путей
-import {path} from "./gulp/config/path.js";
-//Импорт общих плагинов 
+import { path } from "./gulp/config/path.js";
 import { plugins } from "./gulp/config/plugins.js";
 
-// Передае значения в глоб переменную
+// Передает значения в глобальную переменную
 global.app = {
   path: path,
   gulp: gulp,
@@ -13,22 +10,22 @@ global.app = {
 }
 
 // Импорт задач
-import {copy} from "./gulp/tasks/copy.js";
-import {reset} from "./gulp/tasks/reset.js";
-import {html} from "./gulp/tasks/html.js";
-import {server} from "./gulp/tasks/server.js";
-import {scss} from "./gulp/tasks/scss.js";
-import {js} from "./gulp/tasks/js.js";
-import {images} from "./gulp/tasks/images.js";
+import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
+import { server } from "./gulp/tasks/server.js";
+import { scss } from "./gulp/tasks/scss.js";
+import { js } from "./gulp/tasks/js.js";
+import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 
 // Наблюдатель за изменениями в файлах
 function watcher() {
-  gulp.watch(path.watch.files, copy)
-  gulp.watch(path.watch.html, html)
-  gulp.watch(path.watch.scss, scss)
-  gulp.watch(path.watch.js, js)
-  gulp.watch(path.watch.images, images)
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.scss, scss);
+  gulp.watch([path.watch.js, `!${path.srcFolder}/js/background.js`], js);
+  gulp.watch(path.watch.images, images);
 }
 
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
@@ -37,5 +34,11 @@ const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images)
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
-//Выполнение сценария по умолчанию
+// Задача для сборки проекта
+const build = gulp.series(reset, mainTasks);
+
+// Выполнение сценария по умолчанию
 gulp.task('default', dev);
+
+// Экспорт задачи build
+gulp.task('build', build);
